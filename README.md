116557dbbbe8111f3498ab1f221696d5bedf0f0f
Icomanman <fred_barabs@yahoo.com>
2021-09-17 00:07:05 +0800

    setup initial server.

--- README.md
+++ README.md
@@ -4,10 +4,12 @@ Family tree web app project
 
 ## Breaking old history:
 
--  git checkout — orphan latest_branch
--  git add -A
--  git commit -am “Initial commit message” #Committing the changes
--  git branch -D master #Deleting master branch
--  git branch -m master #renaming branch as master
--  git push -f origin master #pushes to master branch
--  git gc — aggressive — prune=all # remove the old files
+-  git count-objects -v
+
+1. git checkout — orphan latest_branch
+2. git add -A
+3. git commit -am “Initial commit message” #Committing the changes
+4. git branch -D master #Deleting master branch
+5. git branch -m master #renaming branch as master
+6. git push -f origin master #pushes to master branch
+7. git gc — aggressive — prune=all # remove the old files
--- init.js
+++ init.js
@@ -1,10 +1,36 @@
 const express = require('express');
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
+const child_process = require('child_process');
 
-const sqlite3 = require('sqlite3').verbose();
+// const sqlite3 = require('sqlite3').verbose();
 // const db = new sqlite3.Database(':memory:');
 
 function main() {
+    const app = express();
+    const port = 1521;
+    const url = `http://localhost:${port}`;
+    app.listen(port, () => {
+        console.log(`Server running on port ${port}.`);
+        console.log(`Host: ${url}`);
+    });
+
+    // Serve Directories:
+    app.use("/dist", express.static(`${__dirname}/dist`));
+    // app.use("/css", express.static(`${__dirname}/dist/css`));
+    app.use("/", express.static(`${__dirname}/dist`));
+
+    let start = "xdg-open";
+    let kill_comm = "pkill -f chrome";
+    if (process.platform == "win32") {
+        start = "start";
+        kill_comm = "'TASKKILL /F /IM chrome.exe /T'";
+    };
+
+    // Kill browser upon node exit
+    process.on('exit', () => child_process.exec(kill_comm));
+    child_process.exec(`${start} ${url}/`);
 
 }
+
+main();
