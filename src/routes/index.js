69d7b06dc6bde643120a9d8fdaa1c65ac937fce4
Icomanman <fred_barabs@yahoo.com>
2021-09-18 23:18:42 +0800

    compressing middlewares.

--- src/routes/index.js
+++ src/routes/index.js
@@ -10,8 +10,17 @@ const global_path = process.cwd();
 const router = express.Router();
 
 module.exports = () => {
+    // homepage (index) as static html:
     router.get('/', express.static(`${global_path}/dist`));
-    router.use('/login', login());
+    // login page:
+    router.use('/login', (req, res) => {
+        if (req.method === 'GET') {
+            res.end();
+            // res.render('tmp', { content: 'login.html' });
+        } else {
+            res.end();
+        }
+    });
     // router.use('/register', register());
     return router;
 };
